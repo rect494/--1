5faf29c9a75cde3aa70a5f36b8ee5c77e5d1c23d
rect494 <terminator.0207.senceles@gmail.com>
2025-09-16 20:37:24 +0900

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+// React 18以降なら createRoot を使う
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
